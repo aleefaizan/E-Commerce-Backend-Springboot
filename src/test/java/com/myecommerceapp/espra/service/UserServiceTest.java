9ac17bd42c096bdd21d791dafba22b7d4015a0b5
Faizan Memon <faizanali7855@gmail.com>
2023-08-29 20:26:15 +0500

    Wrote test cases for register user

---
+++ src/test/java/com/myecommerceapp/espra/service/UserServiceTest.java
@@ -0,0 +1,55 @@
+package com.myecommerceapp.espra.service;
+
+import com.icegreen.greenmail.configuration.GreenMailConfiguration;
+import com.icegreen.greenmail.junit5.GreenMailExtension;
+import com.icegreen.greenmail.util.ServerSetupTest;
+import com.myecommerceapp.espra.api.model.RegistrationBody;
+import com.myecommerceapp.espra.exception.UserAlreadyExistsException;
+import jakarta.mail.Message;
+import jakarta.mail.MessagingException;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UserServiceTest {
+    @RegisterExtension
+    private static GreenMailExtension greenMailExtension = new GreenMailExtension(ServerSetupTest.SMTP)
+            .withConfiguration(GreenMailConfiguration.aConfig().withUser("springboot", "secret"))
+            .withPerMethodLifecycle(true);
+    /** The UserService to test. */
+    @Autowired
+    private UserServiceImpl userService;
+
+    /**
+     * Tests the registration process of the user.
+     * @throws MessagingException Thrown if the mocked email service fails somehow.
+     */
+    @Test
+    @Transactional
+    public void testRegisterUser() throws MessagingException {
+        RegistrationBody body = new RegistrationBody();
+        body.setUsername("UserA");
+        body.setEmail("UserServiceTest$testRegisterUser@junit.com");
+        body.setFirstName("FirstName");
+        body.setLastName("LastName");
+        body.setPassword("MySecretPassword123");
+        Assertions.assertThrows(UserAlreadyExistsException.class,
+                () -> userService.createUser(body), "Username should already be in use");
+
+        body.setUsername("UserServiceTest$testRegisterUser");
+        body.setEmail("UserA@junit.com");
+        Assertions.assertThrows(UserAlreadyExistsException.class,
+                () -> userService.createUser(body), "Email should already be in use");
+
+        body.setEmail("UserServiceTest$testRegisterUser@junit.com");
+        Assertions.assertDoesNotThrow(() -> userService.createUser(body),
+                "User should register successfully.");
+        Assertions.assertEquals(body.getEmail(), greenMailExtension.getReceivedMessages()[0]
+                .getRecipients(Message.RecipientType.TO)[0].toString());
+
+    }
+}
